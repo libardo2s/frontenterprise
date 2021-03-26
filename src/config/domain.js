ea4d3258c99b71532303a4a45a036b9c0539aaf7
Libardo Cuello <libardoii@hotmail.com>
2021-03-25 20:58:53 -0500

    dominio amazon

--- src/config/domain.js
+++ src/config/domain.js
@@ -1,4 +1,5 @@
 const domain = {
-  uri: "http://127.0.0.1:8000/api",
+  //uri: "http://127.0.0.1:8000/api",
+  uri: "http://ec2-3-22-68-55.us-east-2.compute.amazonaws.com/api",
 };
 export default domain;
